76ac48043884274ade5095d4ae09dee45d2185a7
Diogo Marante Pereira <ist186409@lab6p8.localdomain>
2018-10-24 18:47:17 +0100

    foi no fim das minhas ferias estava na praia a andar parei nas ondas do teu corpo e o pordo sol ficou nesse olhar..

--- P1/ficheiros-auxiliares-v2/proj.py
+++ P1/ficheiros-auxiliares-v2/proj.py
@@ -1,5 +1,5 @@
 from search import *
-import math
+import time
 
 # TAI content
 def c_peg  ():
@@ -33,68 +33,73 @@ def move_initial(move):
 def move_final(move):
     return move[1]
 
-"""Models a Solitaire problem as a satisfaction problem.
- A solution cannot have more than 1 peg left on the board."""
-class solitaire(Problem):
-    def __init__(self, board):
-        self.initial = sol_state(board)
-    def actions(self, state):
-        return
-    def result(self, state, action):
-        return
-    def goal_test(self, state):
-        return
 
-    def path_cost(self, c, state1, action, state2):
-        return c + 1
-    def h(self, node):
-        return
+
 """Needed for informed search."""
 class sol_state():
     def __init__(self, board):
         self.board = board
-        self.numberOfPegs = self.countPegs(board)
-
+        
     def __lt__(self, other):
-        self.numberOfPegs < other.numberOfPegs
+        return self.countPegs() > other.countPegs()
     
-    def countPegs():
+    def countPegs(self):
         pegs = 0
-        for line in board:
+        for line in self.board:
             for content in line:
                 if is_peg(content):
                     pegs += 1
+        return pegs
 
-def is_board_move_down(pos, board):
-    return is_peg(board[pos_l(pos) + 1][pos_c(pos)]) and is_empty(board[pos_l(pos) + 2][pos_c(pos)])
+def filterPegs(move) :
+    canMove = []
+    for movements in move:
+        if movements[0] not in canMove:
+            canMove.append(movements[0])
+    return len(canMove)
 
-def is_board_move_right(pos, board):
-    return is_peg(board[pos_l(pos)][pos_c(pos) + 1]) and is_empty(board[pos_l(pos)][pos_c(pos) + 2])
+"""Models a Solitaire problem as a satisfaction problem.
+ A solution cannot have more than 1 peg left on the board."""
+class solitaire(Problem):
 
-def is_board_move_left(pos, board):
-    return is_peg(board[pos_l(pos)][pos_c(pos) - 1]) and is_empty(board[pos_l(pos)][pos_c(pos) - 2])
+    def __init__(self, board):
+        super().__init__(self,board)
+        self.initial = sol_state(board)
 
-def is_board_move_up(pos, board):
-    return is_peg(board[pos_l(pos) - 1][pos_c(pos)]) and is_empty(board[pos_l(pos) - 2][pos_c(pos)])
+    def actions(self, state):
+        return board_moves(state.board)
+
+    def result(self, state, action):
+        return sol_state(board_perform_move(state.board,action))
+
+    def goal_test(self, state):
+        return state.countPegs() == 1
+
+    def path_cost(self, c, state1, action, state2):
+        return c + 1
+
+    def h(self, node):
+        #return  (2*node.state.countPegs() -1 - filterPegs(board_moves(node.state.board)))
+        return (len(node.state.board)*len(node.state.board[0])-len(board_moves(node.state.board)))*(node.state.countPegs() -1)
+ 
+
 
 b1 = [["_","O","O","O","_"], ["O","_","O","_","O"], ["_","O","_","O","_"],
  ["O","_","O","_","_"], ["_","O","_","_","_"]] 
 
 def board_perform_move(board, move):
-    init = move[0]
-    end = move[1]
-    if pos_l(end) == pos_l(init):
-        if end < init:
-            pegEaten = make_pos(pos_l(end), pos_c(end) + 1)
+
+    if pos_l(move_final(move)) == pos_l(move_initial(move)):
+        if move_final(move) < move_initial(move):
+            pegEaten = make_pos(pos_l(move_final(move)), pos_c(move_final(move)) + 1)
         else:
-            pegEaten = make_pos(pos_l(end), pos_c(init) + 1)
+            pegEaten = make_pos(pos_l(move_final(move)), pos_c(move_initial(move)) + 1)
     else:
-        if end < init:
-            pegEaten = make_pos(pos_l(end) + 1, pos_c(end))
+        if move_final(move) < move_initial(move):
+            pegEaten = make_pos(pos_l(move_final(move)) + 1, pos_c(move_final(move)))
         else:
-            pegEaten = make_pos(pos_l(init) + 1, pos_c(end))
-    #pegEaten = make_pos(int(math.fabs(pos_l(end) - pos_l(init))), int(math.fabs(pos_c(end) - pos_c(init))))
-    print(pegEaten)
+            pegEaten = make_pos(pos_l(move_initial(move)) + 1, pos_c(move_final(move)))
+
     newBoard = []
     lines = len(board)
     columns = len(board[0])
@@ -104,43 +109,99 @@ def board_perform_move(board, move):
             pos = make_pos(line, column)
             newLine.append(board[pos_l(pos)][pos_c(pos)])
         newBoard.append(newLine)
-    print(pos_l(init), pos_c(init))
-    newBoard[pos_l(init)][pos_c(init)] = c_empty()
+
+    newBoard[pos_l(move_initial(move))][pos_c(move_initial(move))] = c_empty()
     newBoard[pos_l(pegEaten)][pos_c(pegEaten)] = c_empty()
-    newBoard[pos_l(end)][pos_c(end)] = c_peg()
+    newBoard[pos_l(move_final(move))][pos_c(move_final(move))] = c_peg()
     return newBoard
 
-def board_moves(board):
-    lines = len(board)
-    columns = len(board[0])
+def check_moves(pos,board):
     moves = []
-    for line in range(lines):
-        for column in range(columns):
-            if is_peg(board[line][column]):
-                if line < 2:
-                    print("line < 2", line, column)
-                    if is_board_move_down(make_pos(line, column), board):
-                        print("mv down", line, column)
-                        moves.append([make_pos(line, column), make_pos(line + 2, column)])
-                elif line > lines - 3:
-                    if is_board_move_up(make_pos(line, column), board):
-                        moves.append([make_pos(line, column), make_pos(line, column - 2)])
-                else:
-                    if is_board_move_down(make_pos(line, column), board):
-                        moves.append([make_pos(line, column), make_pos(line + 2, column)])
-                    if is_board_move_up(make_pos(line, column), board):
-                        moves.append([make_pos(line, column), make_pos(line, column - 2)])
-                if column < 2:
-                    if is_board_move_right(make_pos(line, column), board):
-                        moves.append([make_pos(line, column), make_pos(line, column + 2)])
-                elif column > columns - 3:
-                    if is_board_move_left(make_pos(line,column), board):
-                        moves.append([make_pos(line, column), make_pos(line, column - 2)])
-                else:
-                    if is_board_move_right(make_pos(line, column), board):
-                        moves.append([make_pos(line, column), make_pos(line, column + 2)])
-                    if is_board_move_left(make_pos(line,column), board):
-                        moves.append([make_pos(line, column), make_pos(line, column - 2)])
+    if(pos_l(pos)<len(board)-2):
+        check = check_d_moves(pos,board)
+        if (check!=[]):
+            moves.append(check)
+    if(pos_l(pos)>1):
+        check = check_u_moves(pos,board)
+        if (check!=[]):
+            moves.append(check)
+    if(pos_c(pos)<len(board[0])-2):
+        check = check_r_moves(pos,board)
+        if (check!=[]):
+            moves.append(check)
+    if(pos_c(pos)>1):
+        check = check_l_moves(pos,board)
+        if (check!=[]):
+            moves.append(check)
     return moves
 
-print(board_moves(b1))
+
+def check_d_moves(pos,board):
+    if(is_peg(board[pos_l(pos)+1][pos_c(pos)]) and is_empty(board[pos_l(pos)+2][pos_c(pos)]) ):
+        return [pos,make_pos(pos_l(pos)+2,pos_c(pos))]
+    return []
+
+def check_u_moves(pos,board):
+    if(is_peg(board[pos_l(pos)-1][pos_c(pos)]) and is_empty(board[pos_l(pos)-2][pos_c(pos)]) ):
+        return [pos,make_pos(pos_l(pos)-2,pos_c(pos))]
+    return []
+
+def check_r_moves(pos,board):
+    if(is_peg(board[pos_l(pos)][pos_c(pos)+1]) and is_empty(board[pos_l(pos)][pos_c(pos)+2])  ):
+        return [pos,make_pos(pos_l(pos),pos_c(pos)+2)]
+    return []
+    
+def check_l_moves(pos,board):
+    if(is_peg(board[pos_l(pos)][pos_c(pos)-1]) and is_empty(board[pos_l(pos)][pos_c(pos)-2]) ):
+        return [pos,make_pos(pos_l(pos),pos_c(pos)-2)]
+    return []
+
+def board_moves(board):
+    moves = []
+    for i in range(0,len(board)):
+        for j in range(0,len(board[0])):
+            pos = make_pos(i,j)
+            if (is_peg(board[i][j])):
+                moves += check_moves(pos,board)
+               
+    return moves
+
+
+
+m5x5 = [["_","O","O","O","_"],
+        ["O","_","O","_","O"],
+        ["_","O","_","O","_"],
+        ["O","_","O","_","_"],
+        ["_","O","_","_","_"]]
+
+m4x4 = [["O","O","O","X"],
+        ["O","O","O","O"],
+        ["O","_","O","O"],
+        ["O","O","O","O"]]      
+
+m4x5 = [["O","O","O","X","X"],
+        ["O","O","O","O","O"],
+        ["O","_","O","_","O"],
+        ["O","O","O","O","O"]]
+
+m4x6 = [["O","O","O","X","X","X"],
+        ["O","_","O","O","O","O"],
+        ["O","O","O","O","O","O"],
+        ["O","O","O","O","O","O"]] 
+
+def test(board):
+    start = time.time()
+    print("greedy_search: ", greedy_search(solitaire(board)).state.board)
+    end1 = time.time()
+    print("  Time: ",end1 - start)
+
+    print("depth_first_tree_search: ", depth_first_tree_search(solitaire(board)).state.board)
+    end2 = time.time()
+    print("  Time: ",end2 - end1)
+
+    
+    print("astar_search: ", astar_search(solitaire(board)).state.board)
+    end3 = time.time()
+    print("  Time: ",end3 - end2)
+
+test(m5x5)
